584a605ae2fe035ea71d61097d193bafeef0c34d
Tomas Delclaux Rodriguez-Rey <dlxtomas@gmail.com>
2023-11-02 18:59:24 -0400

    flask server added

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request, jsonify
 
 app = Flask(__name__)
 
@@ -6,3 +7,18 @@ app = Flask(__name__)
 def hello_world():
     print("hello")
     return "<p>Hello, World!</p>"
+
+@app.route('/db', methods = ['POST'])
+def get_data():
+
+    ##Send SQL query - TODO
+
+    ##Parse SQL query and Send JSON object back with x and y data for graph - TODO
+    data = dict()
+    data['x']=[1,2,3,4,5]
+    data['y']=[1,2,3,4,5]
+    return jsonify(
+        isError=False,
+        message="Success",
+        statusCode=200,
+        data=data),200
