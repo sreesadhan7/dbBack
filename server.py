15873e467e58a434c3e1e756a1f2f1b8be3e0976
hari7696 <golamari.h@ufl.edu>
2023-11-18 13:45:40 -0500

    added sample usage

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import request, jsonify
 import oracledb
-
+import queries as q
 app = Flask(__name__)
 cs = "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=oracle.cise.ufl.edu)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SID=orcl)))"
 connection = oracledb.connect(
@@ -25,69 +25,7 @@ def get_data():
     ##Connection to db
 
     # ##Query
-    res = cursor.execute("""
-WITH gloabl_atmosphere_temp_yearly AS
-    (SELECT country,
-        EXTRACT(YEAR FROM time_stamp) as time_stamp,
-        ROUND(AVG(temperature_monthly_mean), 2) atm_temp_yearly_mean
-    FROM 
-        Global_Atmosphere_Temperature 
-    GROUP BY 
-        country, EXTRACT(YEAR FROM time_stamp)
-    ),
-
-surface_and_atmosphere as 
-    (SELECT 
-        ga.country, 
-        EXTRACT(YEAR FROM gs.time_stamp) year,
-        atm_temp_yearly_mean atmosphere_temp, 
-        temperature surface_temp
-    FROM 
-        gloabl_atmosphere_temp_yearly ga 
-        INNER JOIN 
-        Global_Surface_Temperature gs 
-        ON ga.country = gs.country AND ga.time_stamp = EXTRACT(YEAR FROM gs.time_stamp)),
-
-year_temps as 
-    (SELECT * FROM surface_and_atmosphere WHERE 
-        country = 'Afghanistan' ORDER BY year
-    ),
-
-year_temp_country_code AS
-    (
-        SELECT 
-            cp.c_code_ISO3 country, 
-            year, 
-            atmosphere_temp, 
-            surface_temp 
-        FROM 
-            year_temps yt 
-            INNER JOIN country_prime cp ON yt.country = cp.country_source
-    ),
-
-YearGroups AS (
-    SELECT 
-        year,
-        country,
-        surface_temp,
-        atmosphere_temp,
-        ceil(((year - MIN(year-1) OVER ())/5)) AS year_group
-    FROM 
-        year_temp_country_code ORDER BY year
-)
---SELECT * FROM YearGroups;
-SELECT 
-    MIN (year) YEAR,
-    avg(surface_temp) avg_surface_temp,
-    avg(atmosphere_temp) avg_atmosphere_Temp
-FROM 
-    YearGroups
-WHERE
-    year > 1960 and year <= 2022
-GROUP BY
-    year_group
-ORDER BY
-    YEAR""")
+    res = cursor.execute(q.mockup_1_1.format('Afghanistan', 5, 1960, 2022))
     
     for row in res:
         if (row[1]):
