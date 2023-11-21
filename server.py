f7914f7c8b3a1ef1da8acca31e3a7d850e789979
Tomas Delclaux Rodriguez-Rey <dlxtomas@gmail.com>
2023-11-20 19:02:47 -0500

    fixed conflicts

--- server.py
+++ server.py
@@ -36,7 +36,8 @@ def get_data():
 
 
     ##Parse SQL query and Send JSON object back with x and y data for graph - TODO
-
+    if not data_db['x'] or not data_db['y1'] or not data_db['y2']:
+        data_db={}
     return jsonify(
         isError=False,
         message="Success",
@@ -61,7 +62,8 @@ def get_data1_2():
         data_db['y1'].append(row[1])
 
     ##Parse SQL query and Send JSON object back with x and y data for graph - TODO
-
+    if not data_db['x'] or not data_db['y1']:
+        data_db={}
     return jsonify(
         isError=False,
         message="Success",
@@ -92,7 +94,8 @@ def get_data1_3():
         data_db['y3'].append(row[3])
         data_db['y4'].append(row[4])
     ##Parse SQL query and Send JSON object back with x and y data for graph - TODO
-
+    if not data_db['x'] or not data_db['y1'] or not data_db['y2'] or not data_db['y2'] or not data_db['y3'] or not data_db['y4']:
+        data_db={}
     return jsonify(
         isError=False,
         message="Success",
@@ -164,7 +167,8 @@ def get_data2_2():
         data_db['y3'].append(row[3])
 
     ##Parse SQL query and Send JSON object back with x and y data for graph - TODO
-
+    if not data_db['x'] or not data_db['y1'] or not data_db['y2'] or not data_db['y3']:
+        data_db={}
     return jsonify(
         isError=False,
         message="Success",
@@ -189,7 +193,8 @@ def get_data2_3():
         data_db['x'].append(row[0])
         data_db['y1'].append(row[1])
         data_db['y2'].append(row[2])
-
+    if not data_db['x'] or not data_db['y1'] or not data_db['y2']:
+        data_db={}
     return jsonify(
         isError=False,
         message="Success",
